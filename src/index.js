ed89289248dcab4002f0d116c73a7cc1a6e54ebc
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-07-14 17:15:27 +0300

    first mutation and query

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-// let's go!
+require('dotenv').config({path: "variables.env"});
+const createServer = require('./createServer'); // dont install as dep
+const db = require('./db');
+
+const server = createServer();
+
+// TODO use express middleware to handle cookies(JWT) and populate current user
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    }
+}, deets => {
+    console.log(`Server RUNNING on http://localhost:${deets.port}/`);
+});
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+const {forwardTo} = require('prisma-binding');
+
+const Query = {
+    items: forwardTo('db'),
+    // this is the same as below. It is because we directly just wanted items, there is nothing extra happening
+    // async items(parent, args, ctx, info) {
+    //     const items = await ctx.db.query.items();
+    //     return items;
+    // }
+};
 
 module.exports = Query;
