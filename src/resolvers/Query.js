72b3e96ff7341948c52c88d32789fc7266934857
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-07-28 20:40:06 +0300

    create query for all users and check whether the user that requested this info has the required permissions

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const {forwardTo} = require('prisma-binding');
+const {hasPermission} = require("../utils");
 
 const Query = {
     items: forwardTo('db'),
@@ -17,6 +18,16 @@ const Query = {
         return ctx.db.query.user({
             where: {id: ctx.request.userId}
         }, info)
+    },
+    async users(parent, args, ctx, info) {
+        // 1. check if user is logged in
+        if (!ctx.request.userId) {
+            throw new Error('You must be logged in!')
+        }
+        // 2. check if the user has the permissions to query all the users
+        hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+        // 3. if they do, query all the users
+        return ctx.db.query.users({}, info) // pass empty where object since we want all users.
     }
 };
 
