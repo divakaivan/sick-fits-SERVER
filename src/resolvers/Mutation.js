3190ad3c8a7b9e3ae6f92d75cadd7be4950210e4
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-07-28 14:02:03 +0300

    add signin resolver

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -47,13 +47,34 @@ const mutations = {
             }
         }, info);
         // create jwt token for the user
-        const token = jwt.sign({userId: user.id}, process.env.APP_SECRET)
+        const token = jwt.sign({userId: user.id}, process.env.APP_SECRET);
         // set the jwt as a cookie on the response so that every time they click on another page, the token comes on the ride
         ctx.response.cookie('token', token, {
             httpOnly: true,
             maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
         });
         // finally return the user to the browser
+        return user;
+    },
+    async signin(parent, {email, password}, ctx, info) {
+        // 1. check if there is a user with that email
+        const user = await ctx.db.query.user({where: {email}});
+        if (!user) {
+            throw new Error(`No such user found for email: ${email}`) // this error is thrown to the frontend and is caught by Query/Mutation components
+        };
+        // 2. check if their password is correct
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) {
+            throw new Error('Invalid password!')
+        }
+        // 3. generate the jwt token
+        const token = jwt.sign({userId: user.id}, process.env.APP_SECRET);
+        // 4. set the cookie with the token
+        ctx.response.cookie('token', token, {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24 * 365
+        });
+        // 5. return the user
         return user;
     }
 };
