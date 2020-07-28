981903dbf54af8f7224fe22964ec787ab2e018e7
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-07-28 13:55:18 +0300

    add the userId to every request.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config({path: "variables.env"});
 const createServer = require('./createServer'); // dont install as dep
 const db = require('./db');
@@ -7,6 +8,17 @@ const server = createServer();
 
 // use express middleware to handle cookies(JWT) and populate current user
 server.express.use(cookieParser());
+
+// decode the jwt so we can get the user id on each request
+server.express.use((req, res, next)=> {
+   const {token} = req.cookies;
+   if (token) {
+       const {userId} = jwt.verify(token, process.env.APP_SECRET);
+       // put the userId onto the request for further request to access
+       req.userId = userId;
+   }
+   next()
+});
 
 server.start({
     cors: {
