cc31de7d83cb8168f66379d4378ac656d97006d4
divakaivan <54508530+divakaivan@users.noreply.github.com>
2020-08-05 18:39:41 +0300

    order and orders query resolvers

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -28,6 +28,27 @@ const Query = {
         hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
         // 3. if they do, query all the users
         return ctx.db.query.users({}, info) // pass empty where object since we want all users.
+    },
+    async order(parent, args, ctx, info) {
+        // 1. make sure they are logged in
+        if (!ctx.request.userId) throw new Error("You aren't logged in!");
+        // 2. query the current order
+        const order = await ctx.db.query.order({
+            where: {id: args.id}
+        }, info);
+        // 3. check if they have the permissions to see this order
+        const ownsOrder = order.user.id === ctx.request.userId;
+        const hasPermissionToSeeOrder = ctx.request.user.permissions.includes("ADMIN");
+        if (!ownsOrder || !hasPermissionToSeeOrder) throw new Error("You cannot see this!")
+        // 4. return the order
+        return order
+    },
+    async orders(parent, args, ctx, info) {
+        const {userId} = ctx.request;
+        if (!userId) throw new Error("You must be logged in !");
+        return ctx.db.query.orders({
+            where: {user: {id: userId}}
+        }, info)
     }
 };
 
